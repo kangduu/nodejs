afbd28494d0ddb0a0bc5a1f1519c14a34d65d7c0
dukang <dk@nosugartech.com>
2024-05-10 18:29:39 +0800

    'class'

--- examples/server.js
+++ examples/server.js
@@ -3,10 +3,10 @@ const { createServer } = require('node:http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = createServer((request, response) => {
-	response.statusCode = 200;
-	response.setHeader('Content-Type', 'text/plain');
-	response.end('Hello Nodejs');
+const server = createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('Hello Nodejs');
 });
 
 server.listen(port, hostname, () => {
