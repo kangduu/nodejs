b77fbc149ac84fe7e5eccb5da651fe14a6f4357d
kangduu <dukang1127@163.com>
2024-12-01 21:35:51 +0800

    Feat：读取本地缓存数据

--- permutation3/index.js
+++ permutation3/index.js
@@ -1,13 +1,13 @@
-const fs = require('fs');
-const fetchData = require('./data.js');
+const fs = require("fs");
+const fetchData = require("./fetch.js");
 
 fetchData()
-	.then(data => {
-		fs.writeFile('data-source.json', JSON.stringify(data), err => {
-			if (err) console.log(err);
-			console.log('The file has been saved!');
-		});
-	})
-	.catch(error => {
-		console.log(error);
-	});
+  .then((data) => {
+    fs.writeFile("data-source.json", JSON.stringify(data), (err) => {
+      if (err) console.log(err);
+      console.log("The file has been saved!");
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
